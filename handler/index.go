3253f3f111948f70b1cae2c7c8a8b386d52aa001
artleules <ardak.tleules11@gmail.com>
2025-06-26 20:07:05 +0500

    use creation date, first album

--- handler/index.go
+++ handler/index.go
@@ -14,10 +14,12 @@ const (
 )
 
 type Artist struct {
-	ID      int      `json:"id"`
-	Image   string   `json:"image"`
-	Name    string   `json:"name"`
-	Members []string `json:"members"`
+	ID           int      `json:"id"`
+	Image        string   `json:"image"`
+	Name         string   `json:"name"`
+	Members      []string `json:"members"`
+	CreationDate int      `json:"creationDate"`
+	FirstAlbum   string   `json:"firstAlbum"`
 }
 
 var artists []Artist
