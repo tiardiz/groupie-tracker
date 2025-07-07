a849274bd963f2ada2f860ad48029b4af626f61e
Mika <akimjedi@yandex.kz>
2025-07-07 20:28:23 +0500

    clean up init function

--- internal/server/init.go
+++ internal/server/init.go
@@ -8,27 +8,37 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 )
 
 func Init(link, port string) {
-	if err := helpers.ChangeDirProjectRoot(); err != nil {
-		log.Fatal(err)
-	}
-	if err := checkStaticFiles(); err != nil {
-		log.Fatal(err)
+	setupErr := prepareEnv()
+	if setupErr != nil {
+		log.Fatal(setupErr)
 	}
 	fs := http.FileServer(http.FS(static.Files))
 	http.HandleFunc("/", handlers.HandleHome)
 	http.HandleFunc("/artist/", handlers.HandleArtist)
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
-	log.Println("Starting server at", link+port)
+	addr := link + port
+	log.Println("Starting server at", addr)
 	err := http.ListenAndServe(port, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
-const staticFilesLocation = "frontend/static/"
+func prepareEnv() error {
+	if err := helpers.ChangeDirProjectRoot(); err != nil {
+		return err
+	}
+	if err := checkStaticFiles(); err != nil {
+		return err
+	}
+	return nil
+}
+
+var staticFilesLocation = filepath.Join("frontend", "static")
 
 var staticFilenames = [...]string{
 	"background.webp",
@@ -40,7 +50,7 @@ var staticFilenames = [...]string{
 
 func checkStaticFiles() error {
 	for _, filename := range staticFilenames {
-		if _, err := os.Stat(staticFilesLocation + filename); err != nil {
+		if _, err := os.Stat(filepath.Join(staticFilesLocation, filename)); err != nil {
 			return fmt.Errorf("cannot access \"%s\": %w", filename, err)
 		}
 	}
